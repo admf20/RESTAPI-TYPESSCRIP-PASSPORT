ea9c37448134101ebe817335e013932f1a7cb5de
Monsaleve <felipe20001monsalve@gmail.com>
2020-04-09 04:12:41 -0500

    Correccion

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export default {
     jwtSecret: process.env.JWT_SECRET || 'somesecrettoken', //configuracion del module para el JWT
-    BD:{                                                    //configuarcion del modelu en la vase de datos
+    BD:{                                                    //configuarcion del modelu en la base de datos
         URI: process.env.MONGODB_URI || 'mongodb://localhost/jwtTutorial',
         USER: process.env.MONGODB_USER,
         PASSWORD: process.env.MONGODB_PASSWORD
